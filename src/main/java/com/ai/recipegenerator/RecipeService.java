1d5a8abaccad3f08fd381ac66caafd660f58cf1f
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2024-09-24 00:23:27 +0530

    update recipe service

--- src/main/java/com/ai/recipegenerator/RecipeService.java
+++ src/main/java/com/ai/recipegenerator/RecipeService.java
@@ -3,8 +3,11 @@ package com.ai.recipegenerator;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.stereotype.Service;
+
 import java.util.Map;
 
+@Service
 public class RecipeService {
     private final ChatModel chatModel;
 
